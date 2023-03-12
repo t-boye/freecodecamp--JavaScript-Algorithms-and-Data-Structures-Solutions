dc8b975b5c6d3e1b393340c0bba890cbc9d88e88
Boye Tete Emmanuel <emmanuelboye1957@gmail.com>
2023-03-12 07:27:08 -0700

    Add files via upload

---
+++ Palindrome Checker.js
@@ -0,0 +1,20 @@
+function palindrome(str) {
+
+    str = str.replace(/[\W_]/g, '').toLowerCase();
+
+    // Check if the string is equal to its reverse format
+    return str === str.split('').reverse().join('');
+}
+
+console.log(palindrome("eye"));
+console.log(palindrome("_eye"));
+console.log(palindrome("race car"));
+console.log(palindrome("not a palindrome"));
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("never odd or even"));
+console.log(palindrome("nope")); // false
+console.log(palindrome("almostomla")); // false
+console.log(palindrome("My age is 0, 0 si ega ym.")); // true
+console.log(palindrome("1 eye for of 1 eye.")); // false
+console.log(palindrome("0_0 (: /- :) 0-0")); // true
+console.log(palindrome("five|\_/|four")); // false
